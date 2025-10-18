9984842067b8ec76d1b59785e006354103f1cbce
Sannidhi.Sunkad <01fe24ica004@kletech.ac.in>
2025-10-18 21:35:00 +0530

    anannyas journal 4

--- operations.py
+++ operations.py
@@ -5,3 +5,8 @@ add = n1+n2
 sub = n1-n2
 print(f"Addition of {n1} and {n2} is :{add}")
 print(f"Subtraction of {n1} and {n2} is :{sub}")
+#multiplication and division of two numbers
+mul = n1*n2
+div = n1/n2 
+print(f"Multiplication of {n1} and {n2} is :{mul}")
+print(f"Division of {n1} and {n2} is :{div}")
