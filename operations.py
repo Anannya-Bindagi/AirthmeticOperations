9edbbdc14fa98b5699ab430a217888253ab77bb5
Anannya <01fe24ica023@kletech.ac.in>
2025-10-18 19:44:57 +0530

    my first file with addition and subtraction operations

---
+++ operations.py
@@ -0,0 +1,7 @@
+#performinf addition and subtraction of two numbers
+n1 = int(input("Enter the first number:"))
+n2 = int(input("Enter the second number:"))
+add = n1+n2
+sub = n1-n2
+print(f"Addition of {n1} and {n2} is :{add}")
+print(f"Subtraction of {n1} and {n2} is :{sub}")
